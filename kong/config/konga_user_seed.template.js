3b490131b95d8b1604fdef0323f9a98bb99f7e31
Rodrigo Lopes <rodnascimentolopes@gmail.com>
2022-09-23 17:28:47 -0300

    updates

---
+++ kong/config/konga_user_seed.template.js
@@ -0,0 +1,15 @@
+const password = ''; // min lengh 6 chars
+const email = '';
+
+module.exports = [
+  {
+    "username": 'admin',
+    "email": `${email}`,
+    "firstName": 'Admin',
+    "lastName": 'Admin',
+    "node_id": 'http://kong:8001',
+    "admin": true,
+    "active" : true,
+    "password": password
+  }
+]
